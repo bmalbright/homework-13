0769825016c268d1a41b77798a6f490a296f3e81
bmalbright <brianalbright@utexas.edu>
2021-10-30 23:19:11 -0500

    finished routes and models.

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,17 +7,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+Product.findAll({
+  include: [Category, Tag]
+})
+.then((products)=>res.json(products))
+.catch((err)=>res.json(err))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id:req.params.id
+    },
+    include: [Category, Tag]
+  })
+  .then((products)=>res.json(products))
+  .catch((err)=>res.json(err))
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
+  /* req.body should look like this...use this for the insomnia post
     {
       product_name: "Basketball",
       price: 200.00,
@@ -91,6 +104,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id:req.params.id
+    }
+  })
+  .then((products)=>res.json(products))
+  .catch((err)=>res.json(err))
 });
 
 module.exports = router;
